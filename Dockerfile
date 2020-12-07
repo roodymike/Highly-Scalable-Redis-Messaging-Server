6cf51a28d64438c3d41b9d9854bc77a7505acc6d
Sumit <sumit.en1997@gmail.com>
2020-12-08 00:22:40 +0530

    Client + and Message server complete

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:14
 WORKDIR /usr/src/app
-COPY package*.json app.js ./
+COPY package*.json app.mjs ./
 RUN npm install
 EXPOSE 12000
-CMD ["node", "app.js"]
+CMD ["node", "app.mjs"]
--- test_server.js
+++ test_server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var process = require('process');
-var data = process.env.NODE_ENV;
+var os = require('os');
+var data = process.env.SERVICE_PORTS || 'Default messagge';
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World! from :- ' + data);
+    res.write('Hello World! from :- ' + os.hostname() + " on port :- " + data);
     res.end();
 }).listen(12000);
